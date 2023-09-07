dfb2ecd19a11bb9b1b9d0c56fb782c43599b3570
Ayoub <hassankoba1999@gmail.com>
2023-09-07 16:37:46 -0700

    py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    
+    import sys
+
+    numb = len(sys.argv) - 1
+    if numb == 0:
+        print("0 arguments.")
+    
+    elif numb == 1:
+        print("1 argument:")
+    
+    else:
+        print("{} arguments:".format(numb))
+    
+    for i in range(numb):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
