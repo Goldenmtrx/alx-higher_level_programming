374e14ed8865a557db4dcb2816ac534c1bfa9d68
Ayoub <hassankoba1999@gmail.com>
2023-09-07 17:25:32 -0700

    py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    
+
     from magic_calculation_102 import add, sub
-    
+
     if a < b:
         somme = add(a, b)
         for i in range(4, 6):
