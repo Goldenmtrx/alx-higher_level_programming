7a3444b60be553b3e3c0abd5d09d9813876945ba
Ayoub <hassankoba1999@gmail.com>
2023-09-07 17:41:02 -0700

    py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
+    """match bytecode provided by H S"""
     from magic_calculation_102 import add, sub
     if a < b:
         s = add(a, b)
         for i in range(4, 6):
             s = add(s, i)
             return (s)
+        
         else:
             return(sub(a, b))
