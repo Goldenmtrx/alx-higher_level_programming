23234fe1b45e4130b0d3c3f71810b84ee5a81f9b
Ayoub <hassankoba1999@gmail.com>
2023-09-26 19:19:08 -0700

    commyy

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
 
+        Args:
+            size (int): size of the new square.
+            position (int, int): position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -21,6 +31,7 @@ class Square:
 
     @property
     def position(self):
+        """set the current position of the square."""
         return (self.__position)
 
     @position.setter
@@ -33,9 +44,11 @@ class Square:
         self.__position = value
 
     def area(self):
+        """Return current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Print square with the # character."""
         if self.__size == 0:
             print("")
             return
@@ -47,6 +60,7 @@ class Square:
             print("")
 
     def __str__(self):
+        """Define print() representation of a Square."""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
