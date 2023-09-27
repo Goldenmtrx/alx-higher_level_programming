47866b876878d2a57392ca8630d2e464a7350d5f
Ayoub <hassankoba1999@gmail.com>
2023-09-26 18:55:54 -0700

    commy

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-""" Define a class Square."""
+"""Define a class Square."""
 
 
 class Square:
-    """A class that represents a square."""
+    """Represent a square."""
     pass
-
